35c21c8bdae704618c6d7a5b15fbfad5f1a3dc08
quat1024 <quat1024@users.noreply.github.com>
2024-08-05 19:00:13 -0400

    Oops deno is buggy

--- src-js/index.mjs
+++ src-js/index.mjs
@@ -1,4 +1,4 @@
-import * as fs from "node:fs/promises";
+import * as fs from "node:fs";
 import * as path from "node:path";
 import * as process from "node:process";
 
@@ -18,42 +18,39 @@ console.log("out:", outDir);
 console.log();
 
 console.log("Checking in/");
-await fs.access(inDir, fs.constants.F_OK);
+fs.accessSync(inDir, fs.constants.F_OK);
 
 console.log("Cleaning out/");
-await fs.rm(outDir, { force: true, recursive: true });
+fs.rmSync(outDir, { force: true, recursive: true });
 //await fs.mkdir(outDir); //https://github.com/denoland/deno/issues/24900
 
 console.log("Copying in/static/ assets");
 let staticDir = path.join(inDir, "static");
-await fs.access(staticDir, fs.constants.F_OK)
-  .then(_ => fs.cp(staticDir, outDir, { recursive: true }), ignore) // dont copy static/ if it doesn't exist
+fs.accessSync(staticDir, fs.constants.F_OK);
+fs.cpSync(staticDir, outDir, { recursive: true }); // dont copy static/ if it doesn't exist
 
 console.log("Reading posts");
 let inPostsDir = path.join(inDir, "posts");
-let posts = await fs.readdir(inPostsDir)
+let posts = await Promise.all(fs.readdirSync(inPostsDir)
   // 👇 yeah readdir just gives you filenames :/
-  .then(filenames => filenames.map(x => path.join(inPostsDir, x)))
-  .then(paths => Promise.all(paths.map(path => new post.Post().read(path))));
+  .map(filename => path.join(inPostsDir, filename))
+  .map(path => new post.Post().read(path)));
 
 let postdb = new post.Db(posts);
 
 console.log("Rendering pages");
-await fs.mkdir(path.join(outDir, "discord"), {recursive: true});
+fs.mkdirSync(path.join(outDir, "discord"), {recursive: true});
 
-await Promise.all([
-  fs.writeFile(path.join(outDir, "index.html"), templates.landing(postdb).renderToString()),
+  fs.writeFileSync(path.join(outDir, "index.html"), templates.landing(postdb).renderToString());
   
-  fs.writeFile(path.join(outDir, "discord.html"), templates.discord().renderToString()), //old location
-  fs.writeFile(path.join(outDir, "discord", "index.html"), templates.discord().renderToString()), //new location
+  fs.writeFileSync(path.join(outDir, "discord.html"), templates.discord().renderToString()); //old location
+  fs.writeFileSync(path.join(outDir, "discord", "index.html"), templates.discord().renderToString()); //new location
   
   
-  ...posts.map(async post => {
+  posts.map(post => {
     let parent = path.join(outDir, "posts", post.slug);
-    await fs.mkdir(parent, {recursive: true});
-    
-    return fs.writeFile(path.join(parent, "index.html"), post.toHtml().renderToString())
-  })
-])
+    fs.mkdirSync(parent, {recursive: true});
+    fs.writeFileSync(path.join(parent, "index.html"), post.toHtml().renderToString())
+  });
 
 console.log("Done");
